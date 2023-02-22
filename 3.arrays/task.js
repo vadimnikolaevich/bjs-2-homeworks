487dc797dadaaba5f35a4dc64411fa906f69ae84
vadimnikolaevich <124394236+vadimnikolaevich@users.noreply.github.com>
2023-02-22 15:09:54 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  return result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
+    return ((arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index]));
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  let result = arr.filter(gender => gender).map(midAge => midAge.age).reduce((acc, item, index, arr) => {
-    acc += item;
-    if(index === arr.length - 1) {
+  let result = users.filter(user => user.gender === gender).map(old => old.age).reduce((acc, old, index, arr) => {
+    acc += old;
+    if (index === arr.length - 1) {
         return acc / arr.length;
     }
     return acc;
-  });
-  
+  }, 0);
+  return result;
 }
