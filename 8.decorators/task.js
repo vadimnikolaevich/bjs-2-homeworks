13205fcd457f772e41b40a098b7e0de9ed28ea0b
vadimnikolaevich <vadimlitvincuk@gmail.com>
2023-02-24 15:42:44 +0300

    8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -19,37 +19,30 @@ function cachingDecoratorNew(func) {
   // Задача № 2
   function debounceDecoratorNew(func, delay) {
     let timeoutId = null;
-    // обертка описана с использованием ключевого слова function и поименована,
-    // поэтому ей можно задать свойства-счетчики до ее описания
+    
     wrapper.allCount = 0;
     wrapper.count = 0;
-    // добавляем фичу подсчета запусков декорируемой функции
-    function countWrapper(...args) {
-      // будет вызов декорируемой функции, надо увеличить счетчик ее вызовов
-      wrapper.count++;
+   function countWrapper(...args) {  
+    wrapper.count++;
       func(...args);
     }
-    // добавляем фичу задерженного запуска и подсчета всех попыток запуска
+   
     function wrapper(...args) {
-      // при каждом вызове декоратора, счетчик всех вызовов увеличится
+      
       wrapper.allCount++;
-      if (wrapper.count !== 0) { // запуск не первый, значит запускаем асинхронно
-        // если timeoutId не null, надо сбросить выполнение по таймауту
+      if (wrapper.count !== 0) { 
         if (timeoutId) {
-          // надо быстро за-null-ить timeoutId,
-          // чтобы не произошло clearTimeout(null)
-          // или очистки очищщенного
+          
           const tempId = timeoutId;
           timeoutId = null;
           clearTimeout(tempId);
         }
         timeoutId = setTimeout(() => {
-          // надо за-null-ить timeoutId,
-          // чтобы не прервалось выполнение countWrapper(...args);
+          
           timeoutId = null;
           countWrapper(...args);
         }, delay);
-      } else { // счетчик на нуле - запуск первый и синхронно
+      } else { 
         countWrapper(...args);
       }
     };
